6a83caeb664ce93e5ae21db46dedeb315cd28815
OrigamiDream <sdy36071@naver.com>
2021-12-12 15:37:59 +0900

    Improve preprocessors for noise reduction

--- preprocessors.py
+++ preprocessors.py
@@ -20,13 +20,15 @@ def create_segment_mask(dicom_filepath, label_filepath, remove_noise_intersectio
         polygons = np.array(annotations[i]['polygon'], dtype=np.int32)
         cv2.fillPoly(mask, [polygons], (255, 255, 255))
 
-    noise_eliminated, segmentation = remove_mask_noise(dcm_image, mask)
+    noise_eliminated, segmentation = remove_mask_noise_statically(dcm_image, mask)
+    mask = segmentation  # override masks with translated segmentation
     combined = noise_eliminated.copy()
     combined[segmentation > 0] = 255
     mask[(segmentation == 0) & (mask > 0)] = 0 if remove_noise_intersection else 127
     return dcm_image, mask, combined, noise_eliminated
 
 
+@DeprecationWarning
 def remove_mask_noise(rgb_img, segmentation):
     gray = cv2.cvtColor(rgb_img, cv2.COLOR_RGB2GRAY)
     _, gray = cv2.threshold(gray, 0, 255, cv2.THRESH_OTSU)
@@ -46,3 +48,18 @@ def remove_mask_noise(rgb_img, segmentation):
     segmentation = segmentation.copy()
     segmentation[gray > 127] = 0
     return src_img, segmentation
+
+
+def remove_mask_noise_statically(rgb_img, segmentation):
+    LEFT_PAD = 142
+
+    M = np.array([[1, 0, (LEFT_PAD / 2 - 20) * -1],
+                  [0, 1, 0]]).astype(np.float32)
+    img = rgb_img.copy()
+    img[0:img.shape[1], 0:LEFT_PAD] = (0, 0, 0)
+    img[0:50, 0:LEFT_PAD + 28] = (0, 0, 0)
+
+    height, width = img.shape[:2]
+    img = cv2.warpAffine(img, M, (width, height))
+    segmentation = cv2.warpAffine(segmentation, M, (width, height))
+    return img, segmentation
